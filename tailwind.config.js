0846dc3896282a95ebc919614ad7449e216fa7af
mmfdDevs <it.mmfd@gmail.com>
2024-11-13 18:18:41 +0500

    Initial commit of Next.js Adventure website

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,43 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container:{
+        center:true,
+        padding:"15px",
+    },
+
+    screens: {
+      sm:"640px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1200px",
+    },
+
+    fontFamily: {
+         primary: "var(--font-bebas)",
+         secondary: "var(--font-inter)",
+    },
+
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+     
+        colors:{
+          primary:"#10151f",
+          secondary:"#475467",
+          backgroundDarkGreen:"#164A41",
+          backgroundMediumGreen:"#277943",
+          backgroundLightGreen:"#369958",
+          titleYellow:"#f6eb00",
+          titleWhite:"#fafaf5",
+          alpha:"#000000",
+          accent:{
+            DEFAULT : "#7edad2",
+            hover:"#79d3cc",
+
+          },
+        },
+     boxShadow: {
+      custom: "0px 14px 54px rgba(0,0,0,0.1)",
+     }
     },
   },
   plugins: [],
